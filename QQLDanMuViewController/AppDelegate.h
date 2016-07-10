b6c51f54bd10a35e91b864b4aa6658b3dcdf3f4a
lumouren009 <luzheng1208@163.com>
2016-07-10 13:02:09 +0800

    Initial Commit

---
+++ QQLDanMuViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQLDanMuViewController
+//
+//  Created by 卢政 on 16/7/10.
+//  Copyright © 2016年 卢政. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQLDanMuViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQLDanMuViewController
+//
+//  Created by 卢政 on 16/7/10.
+//  Copyright © 2016年 卢政. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
